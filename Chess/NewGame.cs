26f6589ed3e5d1f21598e9d947435349e58f02b4
KingofPawns <dvandeveer@student.neumont.edu>
2018-08-01 15:21:28 -0600

    Updated using visual studio 2017, added chess 960 checkbox to new game window

--- Chess/NewGame.cs
+++ Chess/NewGame.cs
@@ -33,10 +33,12 @@ namespace Chess
 		public System.Windows.Forms.TextBox BlackPlayerName;
 		public System.Windows.Forms.TextBox WhitePlayerName;
 		public System.Windows.Forms.PictureBox WhitePlayerImage;
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
+        private CheckBox checkBox1;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
 
 		public NewGame()
 		{
@@ -89,6 +91,7 @@ namespace Chess
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.WhitePlayerName = new System.Windows.Forms.TextBox();
             this.WhitePlayerImage = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -103,27 +106,27 @@ namespace Chess
             this.groupBox1.Controls.Add(this.PlayersCvC);
             this.groupBox1.Controls.Add(this.PlayersHvC);
             this.groupBox1.Controls.Add(this.PlayesrHvH);
-            this.groupBox1.Location = new System.Drawing.Point(24, 24);
+            this.groupBox1.Location = new System.Drawing.Point(48, 44);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(160, 96);
+            this.groupBox1.Size = new System.Drawing.Size(320, 178);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Player Options";
             // 
             // PlayersCvC
             // 
-            this.PlayersCvC.Location = new System.Drawing.Point(14, 70);
+            this.PlayersCvC.Location = new System.Drawing.Point(28, 129);
             this.PlayersCvC.Name = "PlayersCvC";
-            this.PlayersCvC.Size = new System.Drawing.Size(146, 16);
+            this.PlayersCvC.Size = new System.Drawing.Size(292, 30);
             this.PlayersCvC.TabIndex = 2;
             this.PlayersCvC.Text = "Computer Vs. Computer";
             this.PlayersCvC.CheckedChanged += new System.EventHandler(this.PlayesrType_CheckedChanged);
             // 
             // PlayersHvC
             // 
-            this.PlayersHvC.Location = new System.Drawing.Point(14, 47);
+            this.PlayersHvC.Location = new System.Drawing.Point(28, 87);
             this.PlayersHvC.Name = "PlayersHvC";
-            this.PlayersHvC.Size = new System.Drawing.Size(136, 20);
+            this.PlayersHvC.Size = new System.Drawing.Size(272, 37);
             this.PlayersHvC.TabIndex = 1;
             this.PlayersHvC.Text = "Human Vs. Computer";
             this.PlayersHvC.CheckedChanged += new System.EventHandler(this.PlayesrType_CheckedChanged);
@@ -131,9 +134,9 @@ namespace Chess
             // PlayesrHvH
             // 
             this.PlayesrHvH.Checked = true;
-            this.PlayesrHvH.Location = new System.Drawing.Point(14, 24);
+            this.PlayesrHvH.Location = new System.Drawing.Point(28, 44);
             this.PlayesrHvH.Name = "PlayesrHvH";
-            this.PlayesrHvH.Size = new System.Drawing.Size(136, 20);
+            this.PlayesrHvH.Size = new System.Drawing.Size(272, 37);
             this.PlayesrHvH.TabIndex = 0;
             this.PlayesrHvH.TabStop = true;
             this.PlayesrHvH.Text = "Human Vs. Human";
@@ -145,35 +148,35 @@ namespace Chess
             this.groupBox2.Controls.Add(this.PlayerLevel3);
             this.groupBox2.Controls.Add(this.PlayerLevel2);
             this.groupBox2.Controls.Add(this.PlayerLevel1);
-            this.groupBox2.Location = new System.Drawing.Point(24, 125);
+            this.groupBox2.Location = new System.Drawing.Point(48, 231);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(160, 91);
+            this.groupBox2.Size = new System.Drawing.Size(320, 168);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Player Level";
             // 
             // PlayerLevel3
             // 
-            this.PlayerLevel3.Location = new System.Drawing.Point(13, 64);
+            this.PlayerLevel3.Location = new System.Drawing.Point(26, 118);
             this.PlayerLevel3.Name = "PlayerLevel3";
-            this.PlayerLevel3.Size = new System.Drawing.Size(139, 20);
+            this.PlayerLevel3.Size = new System.Drawing.Size(278, 37);
             this.PlayerLevel3.TabIndex = 2;
             this.PlayerLevel3.Text = "Chess Master";
             // 
             // PlayerLevel2
             // 
-            this.PlayerLevel2.Location = new System.Drawing.Point(13, 44);
+            this.PlayerLevel2.Location = new System.Drawing.Point(26, 81);
             this.PlayerLevel2.Name = "PlayerLevel2";
-            this.PlayerLevel2.Size = new System.Drawing.Size(139, 20);
+            this.PlayerLevel2.Size = new System.Drawing.Size(278, 37);
             this.PlayerLevel2.TabIndex = 1;
             this.PlayerLevel2.Text = "Intermediate";
             // 
             // PlayerLevel1
             // 
             this.PlayerLevel1.Checked = true;
-            this.PlayerLevel1.Location = new System.Drawing.Point(13, 24);
+            this.PlayerLevel1.Location = new System.Drawing.Point(26, 44);
             this.PlayerLevel1.Name = "PlayerLevel1";
-            this.PlayerLevel1.Size = new System.Drawing.Size(139, 20);
+            this.PlayerLevel1.Size = new System.Drawing.Size(278, 37);
             this.PlayerLevel1.TabIndex = 0;
             this.PlayerLevel1.TabStop = true;
             this.PlayerLevel1.Text = "Beginner";
@@ -183,18 +186,18 @@ namespace Chess
             this.groupBox3.BackColor = System.Drawing.Color.Transparent;
             this.groupBox3.Controls.Add(this.BlackPlayerName);
             this.groupBox3.Controls.Add(this.BlackPlayerImage);
-            this.groupBox3.Location = new System.Drawing.Point(192, 24);
+            this.groupBox3.Location = new System.Drawing.Point(384, 44);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(156, 95);
+            this.groupBox3.Size = new System.Drawing.Size(312, 176);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Black Player";
             // 
             // BlackPlayerName
             // 
-            this.BlackPlayerName.Location = new System.Drawing.Point(67, 40);
+            this.BlackPlayerName.Location = new System.Drawing.Point(134, 74);
             this.BlackPlayerName.Name = "BlackPlayerName";
-            this.BlackPlayerName.Size = new System.Drawing.Size(80, 20);
+            this.BlackPlayerName.Size = new System.Drawing.Size(160, 31);
             this.BlackPlayerName.TabIndex = 0;
             this.BlackPlayerName.Text = "Black Player";
             this.BlackPlayerName.Click += new System.EventHandler(this.PlayerName_Focus);
@@ -204,27 +207,27 @@ namespace Chess
             // 
             this.BlackPlayerImage.BackColor = System.Drawing.Color.Transparent;
             this.BlackPlayerImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.BlackPlayerImage.Location = new System.Drawing.Point(14, 24);
+            this.BlackPlayerImage.Location = new System.Drawing.Point(28, 44);
             this.BlackPlayerImage.Name = "BlackPlayerImage";
-            this.BlackPlayerImage.Size = new System.Drawing.Size(45, 50);
+            this.BlackPlayerImage.Size = new System.Drawing.Size(90, 93);
             this.BlackPlayerImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.BlackPlayerImage.TabIndex = 1;
             this.BlackPlayerImage.TabStop = false;
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(277, 230);
+            this.btnStart.Location = new System.Drawing.Point(554, 425);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(71, 24);
+            this.btnStart.Size = new System.Drawing.Size(142, 44);
             this.btnStart.TabIndex = 5;
             this.btnStart.Text = "&Start";
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(197, 230);
+            this.btnCancel.Location = new System.Drawing.Point(394, 425);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(71, 24);
+            this.btnCancel.Size = new System.Drawing.Size(142, 44);
             this.btnCancel.TabIndex = 4;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
@@ -234,18 +237,18 @@ namespace Chess
             this.groupBox4.BackColor = System.Drawing.Color.Transparent;
             this.groupBox4.Controls.Add(this.WhitePlayerName);
             this.groupBox4.Controls.Add(this.WhitePlayerImage);
-            this.groupBox4.Location = new System.Drawing.Point(192, 124);
+            this.groupBox4.Location = new System.Drawing.Point(384, 229);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(156, 92);
+            this.groupBox4.Size = new System.Drawing.Size(312, 170);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "White Player";
             // 
             // WhitePlayerName
             // 
-            this.WhitePlayerName.Location = new System.Drawing.Point(67, 40);
+            this.WhitePlayerName.Location = new System.Drawing.Point(134, 74);
             this.WhitePlayerName.Name = "WhitePlayerName";
-            this.WhitePlayerName.Size = new System.Drawing.Size(80, 20);
+            this.WhitePlayerName.Size = new System.Drawing.Size(160, 31);
             this.WhitePlayerName.TabIndex = 0;
             this.WhitePlayerName.Text = "White Player";
             this.WhitePlayerName.Click += new System.EventHandler(this.PlayerName_Focus);
@@ -255,18 +258,29 @@ namespace Chess
             // 
             this.WhitePlayerImage.BackColor = System.Drawing.Color.Transparent;
             this.WhitePlayerImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.WhitePlayerImage.Location = new System.Drawing.Point(14, 24);
+            this.WhitePlayerImage.Location = new System.Drawing.Point(28, 44);
             this.WhitePlayerImage.Name = "WhitePlayerImage";
-            this.WhitePlayerImage.Size = new System.Drawing.Size(45, 50);
+            this.WhitePlayerImage.Size = new System.Drawing.Size(90, 93);
             this.WhitePlayerImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.WhitePlayerImage.TabIndex = 1;
             this.WhitePlayerImage.TabStop = false;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(76, 439);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 29);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Chess960";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // NewGame
             // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.AutoScaleBaseSize = new System.Drawing.Size(10, 24);
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(374, 284);
+            this.ClientSize = new System.Drawing.Size(707, 481);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnStart);
@@ -290,6 +304,7 @@ namespace Chess
             this.groupBox4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.WhitePlayerImage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 		#endregion
